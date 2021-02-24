ed57ab9a892b5aa287461f14e0f9eed5f910cfff
xlm <2631583445@qq.com>
2021-02-24 09:18:18 +0800

    完成了主页功能

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$(function () {
+    // 获取用户信息及其权限
+    getUser()
+
+    // 退出按钮
+    $('#btnout').click(function () {
+        // 设置询问退出框
+        layer.confirm('确认退出?', { icon: 3, title: '提示' }, function (index) {
+            // 删除缓存的token信息
+            localStorage.removeItem('token')
+            // 返回到登录界面
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+
+function getUser() {
+    $.ajax({
+        type: "GET",
+        url: '/my/userinfo',
+        success(res) {
+            console.log(this);
+            if (res.status !== 0) return layer.msg('res.message')
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    const username = user.nickname || user.username
+    $('#welcome').html('欢迎' + username)
+    if (user.user_pic) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        const firstName = username[0].toUpperCase()
+        $('.text-avatar').html(firstName).show()
+    }
+}
--- assets/js/link_api.js
+++ assets/js/link_api.js
@@ -2,5 +2,22 @@
 // 在这个函数中，可以拿到我们给ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
     // 在发起真正的ajax请求之前，同一拼接请求的根路径
-    options.url = 'http://ajax.frontend.itheima.net'+options.url
+    options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+    // 统一为有权限的接口设置名叫headers的请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.token || ''
+        }
+
+        const successFunction = options.success.bind(this)
+        options.success = function (res) {
+            successFunction(res)
+            const { message, status } = res
+            if (message === "身份认证失败！" && status === 1) {
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
+        }
+    }
 })
